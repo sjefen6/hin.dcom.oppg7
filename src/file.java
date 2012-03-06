f9c0064f81fbcac01a62a3f5e226f1148b65bb9d
Vegard Langås <sjefen6@gmail.com>
2012-03-06 15:31:48 +0100

    yeah!

--- src/file.java
+++ src/file.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class file {
 	private File file;
@@ -9,9 +15,30 @@ public class file {
 		md5Hash = getMd5Hash(file);
 	}
 
-	private String getMd5Hash(File file2) {
-		// TODO Auto-generated method stub
-		return null;
+	private String getMd5Hash(File file2) {	
+		byte[] buffer = new byte[8192];
+		int read = 0;
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			InputStream is = new FileInputStream(file2);
+			while( (read = is.read(buffer)) > 0) {
+				digest.update(buffer, 0, read);
+			}		
+			byte[] md5sum = digest.digest();
+			BigInteger bigInt = new BigInteger(1, md5sum);
+			String output = bigInt.toString(16);
+			is.close();
+			return output;
+		}
+		catch(IOException e) {
+			return "false";
+		} catch (NoSuchAlgorithmException e) {
+			return "false";
+		}
+	}
+	
+	public String toString(){
+		return md5Hash + "\t" + file.getPath() + "\n";
 	}
 	
 	
---
+++ src/tester.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+
+public class tester {
+	public static void main(String[] args) {
+		file fil = new file(new File("README.md"));
+		System.out.println(fil);
+
+	}
+
+}
