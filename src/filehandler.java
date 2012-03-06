8fed7d5d9cea6f26485e74c441e2e8d1cfbb8a9e
Vegard Langås <sjefen6@gmail.com>
2012-03-06 16:52:25 +0100

    added filewriter

--- src/filehandler.java
+++ src/filehandler.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+
+import javax.swing.JFileChooser;
 
 public class filehandler {
 	private ArrayList<file> fileArray;
@@ -15,6 +19,7 @@ public class filehandler {
 	}
 
 	public filehandler(File md5file) {
+		fileArray = new ArrayList<file>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(md5file));
 
@@ -28,6 +33,47 @@ public class filehandler {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	public boolean verifyMd5Hashes() {
+		for (file f : fileArray) {
+			if (!f.isMD5Valid()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/*
+	 * Based on:
+	 * https://github.com/narvik-studentradio/Playlist-Fucker/blob/master
+	 * /src/FileHandeler.java
+	 */
+	public boolean save() {
+		JFileChooser chooser = new JFileChooser();
+
+		int status = chooser.showSaveDialog(null);
+
+		// Hvis brukeren ikke velger en plassering og filnavn
+		if (status != JFileChooser.APPROVE_OPTION) {
+			return false;
+		} else { // Hvis brukeren velger en plassering og filnavn
+			File outFile = chooser.getSelectedFile();
+			try { // Fors¿ker Œ skrive filen
+				BufferedWriter fileOut = new BufferedWriter(new FileWriter(
+						outFile));
+				fileOut.write(getFile());
+				fileOut.flush();
+				fileOut.close();
+				return true;
+			} catch (IOException e) { // Hvis det feiler så har jeg ikke mye å
+										// fåreslå, kanskje programmet ikke har
+										// skrivetilgang
+				// Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
 		}
 	}
 
--- src/tester.java
+++ src/tester.java
@@ -1,10 +1,11 @@
+import java.io.File;
+
 
 public class tester {
 	public static void main(String[] args) {
-		filehandler fh = new filehandler("lol");
+		filehandler fh = new filehandler(new File("lol.md5"));
 		
-		
-		System.out.println(fh.getFile());
+		System.out.println(fh.verifyMd5Hashes());
 
 	}
 
