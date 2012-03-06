3756714595534204e821935393dde9b04bcdf3b8
Vegard Langås <sjefen6@gmail.com>
2012-03-06 15:04:32 +0100

    Added file and basic filehandeler

---
+++ src/file.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class file {
+	private File file;
+	private String md5Hash;
+	
+	public file(File file){
+		this.file = file;
+		md5Hash = getMd5Hash(file);
+	}
+
+	private String getMd5Hash(File file2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
---
+++ src/filehandler.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class filehandler {
+	private ArrayList<file> fileArray;
+	
+	public filehandler(String location){
+		fileArray.addAll(scan(new File(location)));
+	}
+	
+	
+	/*
+	 * Based on:
+	 * https://github.com/narvik-studentradio/Music-Player/blob/master/src/contentCollection.java
+	 */
+	private Collection<? extends file> scan(File path)
+	{
+		/*
+		 * Lets pork out on ArrayLists
+		 */
+		ArrayList<file> tempSongs = new ArrayList<file>();
+		File[] listOfFiles = path.listFiles();
+
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	        if (listOfFiles[i].isFile()) {
+	          tempSongs.add(new file(listOfFiles[i]));
+	        } else if (listOfFiles[i].isDirectory()) {
+	          tempSongs.addAll(scan(listOfFiles[i]));
+	        }
+	      }
+		return tempSongs;
+	}
+
+}
